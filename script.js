f4b2a36253b88d88b7b93c2ecbb596043298c27a
WhiteSnake <nikhilkr2604@gmail.com>
2023-07-14 16:57:04 +0530

    Done

---
+++ script.js
@@ -0,0 +1,37 @@
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = " ";
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == '+/-'){
+            let num = parseFloat(string);
+            string = -num;
+            document.querySelector('input').value = string
+        }
+        else if(e.target.innerHTML == '()'){
+            if(string.includes("(")){
+                string = string + ")";
+            }
+            else{
+                string = string + "(";
+            }
+            document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == '<i class="fa-solid fa-delete-left" style="color: #000000;"></i>'){
+            string = string.slice(0,-1);
+            document.querySelector('input').value = string;
+        }
+        else{
+        console.log(e.target);
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+    })
+})
